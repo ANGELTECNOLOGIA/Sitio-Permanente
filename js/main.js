375040f7e50bab655c9c26fbe99ce509b5363868
angel <82600987+ANGELTECNOLOGIA@users.noreply.github.com>
2023-05-23 19:10:08 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,2 @@
+var nombre = prompt("¿Cuál es tu nombre?");
+alert("Hola " + nombre + ". En este sitio encontrarás contenido relacionado a varias de las cosas que me gustan, como la formula 1, entre otras cosas más, descubrelo por ti mismo :)");
